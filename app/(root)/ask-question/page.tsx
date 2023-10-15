3ab9784c9b653fc2922c5620b71fed23b0dc0837
Nicolas Soroka <sorokanicolas@gmail.com>
2023-10-15 02:40:36 -0300

    right sidebar ok

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const page = () => {
-  return <div>ask question</div>;
-};
+  return (
+    <div>Ask Question</div>
+  )
+}
 
-export default page;
+export default page
